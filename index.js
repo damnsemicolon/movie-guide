e1dba73e484713f34f049490cc4d18617fc81c1f
damnsemicolon <shannoninengland@gmail.com>
2023-03-29 17:06:57 +0100

    added imdb icon and pulling info from JSON

--- index.js
+++ index.js
@@ -5,6 +5,36 @@ let result = document.getElementById("result");
 // function to fetch data from api
 
 let getMovie = () => {
-    let movieName = movieNameRef.ariaValueMax;
+    let movieName = movieNameRef.value;
     let url = `http://www.omdbapi.com/?t=${movieName}^apikey=${key}`;
+
+
+    // if input field is empty
+    if (movieName.length <= 0) {
+        result.innerHTML = `<h3 class="msg">Please enter a movie name </h3>`;
+    }
+
+    // if input isn't empty
+    else {
+        fetch(url).then((resp) => resp.json()).then((data) => {
+            if (data.Response == "True") {
+                result.innerHTML = `
+                    <div class="info">
+                        <img src=${data.Poster} class="poster">
+                        <div>
+                            <h2>${data.Title}</h2>
+                            <div class="rating">
+                                <img src="IMDB.svg">
+                                <h4>${data.ImdbRating}</h4>
+                        </div>
+                        <div class="details">
+                            <span>${data.Rated}</span>
+                            <span>${data.Year}</span>
+                            <span>${data.Runtime}</span>
+                        </div>
+                        
+                `
+            }
+        })
+    }
 }
